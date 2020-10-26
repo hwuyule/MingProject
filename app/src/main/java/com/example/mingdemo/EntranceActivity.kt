831472b56eafffabf4ffbd3ad94fd20810a76431
huangheming@kuaicom.com <huangheming@kuaishou.com>
2020-10-26 23:45:39 +0800

    增加入口Activity

---
+++ app/src/main/java/com/example/mingdemo/EntranceActivity.kt
@@ -0,0 +1,39 @@
+package com.example.mingdemo
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+
+class EntranceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entrance)
+
+        val linearLayout = findViewById<LinearLayout>(R.id.root)
+
+        /**
+         * 学习一下这个写demo的方法，迅速写多个Activity的入口
+         */
+        packageManager.getPackageInfo(
+            packageName, PackageManager.GET_ACTIVITIES).activities.forEach { activity ->
+            if (activity.name == this::class.java.name) {
+                return@forEach
+            }
+
+            val clazz = Class.forName(activity.name)
+
+            val button = Button(this).apply {
+                isAllCaps = false
+                text = clazz.simpleName
+                setOnClickListener {
+                    startActivity(Intent(this@EntranceActivity, clazz))
+                }
+            }
+            linearLayout.addView(button)
+        }
+    }
+}
--- app/src/main/java/com/example/mingdemo/MainActivity.kt
+++ app/src/main/java/com/example/mingdemo/MainActivity.kt
@@ -2,13 +2,32 @@ package com.example.mingdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 
+/**
+ * TransitionManager简单使用
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val root = findViewById<ViewGroup>(R.id.root)
+        val icon = findViewById<View>(R.id.icon)
+        icon.setOnClickListener {
+            v ->
+            TransitionManager.beginDelayedTransition(root)
+            val params = v.layoutParams as LinearLayout.LayoutParams
+            params.height *= 2
+            params.width *= 2
+            params.gravity = Gravity.CENTER
+            v.layoutParams = params
+        }
 
     }
 }
