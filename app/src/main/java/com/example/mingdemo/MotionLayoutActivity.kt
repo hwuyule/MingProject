474e73e0339d8fef8294c90f7887deb4838605b0
huangheming@kuaicom.com <huangheming@kuaishou.com>
2020-11-02 01:00:31 +0800

    MotionLayout的一些使用demo

---
+++ app/src/main/java/com/example/mingdemo/MotionLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mingdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MotionLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_layout)
+    }
+}
